872f40ffa0c13189a67b35ff0287f947769b3787
srksonwarsha <135790183+srksonwarsha@users.noreply.github.com>
2024-07-03 16:47:23 +0530

    updates

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -13,11 +13,11 @@ export const authSlice = createSlice({
             state.token = action.payload.token;
         },
         setUserDetail(state, action) {
-            state.detail = action.payload.user;
+            state.detail = action.payload;
         },
         clearUser(state) {
             state.token = null;
-            state.detail = null;
+            state.detail = {};
             state.isLoggedIn = false
         },
     },
